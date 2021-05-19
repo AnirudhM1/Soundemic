7ff5cbddd22f331b4c8faa6bf23003c1a79eecc2
Anirudh Mukherjee <mukherjeeanirudh100@gmail.com>
2021-05-19 16:13:14 +0530

    Created the basic layout of the search page

--- routes/Search.js
+++ routes/Search.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    //res.render('search', { songs });
-    res.send('Search Page');
+    res.render('Search', { songs });
 })
 
 // For testing
@@ -19,22 +18,26 @@ const songs = [
     {
         _id: 'song',
         name: 'Lateralus',
-        artist: 'Tool'
+        artist: 'Tool',
+        img_url: 'https://upload.wikimedia.org/wikipedia/en/6/63/Tool_-_Lateralus.jpg'
     },
     {
         _id: 'song2',
         name: 'New Divide',
-        artist: 'Linkin Park'
+        artist: 'Linkin Park',
+        img_url: 'https://upload.wikimedia.org/wikipedia/en/b/b1/New_Divide_%28Linkin_Park_single_-_cover_art%29.jpg'
     },
     {
         _id: 'song',
         name: 'song3',
-        artist: 'artist3'
+        artist: 'artist3',
+        img_url: 'https://upload.wikimedia.org/wikipedia/en/6/63/Tool_-_Lateralus.jpg'
     },
     {
         _id: 'song2',
         name: 'song4',
-        artist: 'artist4'
+        artist: 'artist4',
+        img_url: 'https://upload.wikimedia.org/wikipedia/en/b/b1/New_Divide_%28Linkin_Park_single_-_cover_art%29.jpg'
     }
 
 ]
