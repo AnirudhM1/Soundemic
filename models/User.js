5896b3616376abf1e23a44ce5753d4c1498a25e4
priyansh71 <f20201388@goa.bits-pilani.ac.in>
2021-05-27 00:48:53 +0530

    edited slightly

--- models/User.js
+++ models/User.js
@@ -9,36 +9,36 @@ const userSchema = new Schema({
         required: true
     },
     username: {
-        type: String, 
+        type: String,
         required: true,
         unique: [true, 'username already selected']
     },
     password: { // Saves the hashed password with salt
-        type: String, 
+        type: String,
         required: true
     },
 
-    playlists: [{type: Schema.Types.ObjectId, ref: 'Playlist', required: true }],
-    recents: [{type: Schema.Types.ObjectId, ref: 'Song', required: true}]
+    playlists: [{ type: Schema.Types.ObjectId, ref: 'Playlist', required: true }],
+    recents: [{ type: Schema.Types.ObjectId, ref: 'Song', required: true }]
 });
 
-userSchema.statics.findAndValidate = async function(username, password) {
+userSchema.statics.findAndValidate = async function (username, password) {
     const user = await User.findOne({ username });
-    if(!user) {
+    if (!user) {
         return false
     }
     const isValid = await bcrypt.compare(password, user.password);
     return (isValid) ? user : false;
 }
 
-userSchema.pre('save', async function(next) {
-    if(!this.isModified('password')) return next();
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 12);
     next();
 })
 
-userSchema.post('findOneAndDelete', async function(user) {
-    if(user.playlists.length) {
+userSchema.post('findOneAndDelete', async function (user) {
+    if (user.playlists.length) {
         await Playlist.deleteMany({ _id: { $in: user.playlists } })
     }
 })
